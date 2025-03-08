99221d8fd6bad13612678b9d31756b76c505b2e2
zhoucheng <zhoucheng133@outlook.com>
2025-03-08 22:42:08 +0800

    添加标题栏

---
+++ src/components/title_bar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="head_img">
+      <img src="/icon.svg" alt="" width="40px">
+      <div class="head_label">AnimeHelper</div>
+    </div>
+
+    <div class="menus" v-if="store().token.length!=0">
+      菜单!
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import store from '../store';
+</script>
+
+<style scoped>
+.head_label{
+  margin-left: 15px;
+  font-size: 18px;
+}
+.header{
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  border-bottom: 2px solid #f0f0f0;
+  z-index: 10;
+  position: sticky;
+  justify-content: space-between;
+  user-select: none;
+  height: 60px;
+}
+.head_img{
+  margin-left: 30px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+
+export default defineStore("main", ()=>{
+  const token="";
+
+  return {token};
+})
