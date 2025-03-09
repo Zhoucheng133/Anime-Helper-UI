4b9acb8f32249b1a2cfe4d866ccc62527ff6f444
zhoucheng <zhoucheng133@outlook.com>
2025-03-10 00:23:50 +0800

    添加注销提示框

--- src/components/title_bar.vue
+++ src/components/title_bar.vue
@@ -11,7 +11,7 @@
       <Button label="下载器" variant="text" severity="secondary" @click="toRoute('/downloader')" :disabled="route.path=='/downloader'" icon="pi pi-download"/>
     </div>
     <div class="signout_area">
-      <Button icon="pi pi-sign-out" class="signout" variant="text" severity="danger" v-if="!(store().token.length==0) && !mobile" @click="logoutHandler" />
+      <Button icon="pi pi-sign-out" class="signout" variant="text" severity="danger" v-if="!(store().token.length==0) && !mobile" @click="logoutHandler($event)" />
       <Button icon="pi pi-bars" class="signout" variant="text" v-if="!(store().token.length==0) && mobile" @click="menuHandler" />
       <Menu ref="menu" id="overlay_menu" :model="menuItems" :popup="true" />
     </div>
@@ -20,7 +20,7 @@
 
 <script lang="ts" setup>
 import store from '../store';
-import { Button, Menu } from 'primevue';
+import { Button, Menu, useConfirm } from 'primevue';
 import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -58,7 +58,10 @@ const menuItems=ref([
       {
         label: '注销',
         icon: 'pi pi-sign-out',
-        command: ()=>logoutHandler()
+        command: ()=>{
+          localStorage.clear();
+          window.location.href="/";
+        }
       }
     ]
   }
@@ -74,10 +77,29 @@ const menu = ref();
 function menuHandler(event: any){
   menu.value.toggle(event);
 }
+const confirm = useConfirm();
 
-function logoutHandler(){
-  localStorage.clear();
-  window.location.href="/";
+function logoutHandler(event: any){
+  confirm.require({
+    target: event.currentTarget,
+    message: '你确定要注销吗?',
+    rejectProps: {
+      label: '取消',
+      severity: 'secondary',
+      outlined: true,
+      size: "small"
+    },
+    acceptProps: {
+      label: '注销',
+      size: "small",
+      severity: 'danger',
+    },
+    accept: () => {
+      localStorage.clear();
+      window.location.href="/";
+    },
+    reject: () => {}
+  });
 }
 
 function calWidth(){
