f6096d6cb7fd9fc4bc5b7c28a1208bb147073b56
zhoucheng <zhoucheng133@outlook.com>
2025-03-13 20:34:11 +0800

    添加显示所有列表

--- src/pages/all.vue
+++ src/pages/all.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    所有
+  <div class="page">
+    <DataTable :value="all().list">
+      <Column field="title" header="标题">
+        <template #body="slotProps">
+          <div class="title">
+            {{ slotProps.data.title }}
+          </div>
+        </template>
+      </Column>
+      <Column header="操作" style="min-width: 150px;">
+        <template #body="slotProps">
+          <ButtonGroup>
+            <Button severity="secondary" label="添加至" size="small" style="font-size: 12px;" />
+            <Button severity="secondary" size="small" ><i class="pi pi-download" style="font-size: 12px;"></i></Button>
+          </ButtonGroup>
+        </template>
+      </Column>
+    </DataTable>
   </div>
+  <Loading ref="loadingRef" />
 </template>
+
+<script lang="ts" setup >
+import { DataTable, Column, Button, ButtonGroup } from 'primevue';
+import all from '../store/all';
+import { onMounted, ref } from 'vue';
+import Loading from '../components/loading.vue';
+const loadingRef=ref();
+
+document.title="AnimeHelper | 所有";
+
+onMounted(async ()=>{
+  loadingRef.value.loadingHandler(true, "获取所有列表");
+  await all().getList();
+  loadingRef.value.loadingHandler(false, "获取所有列表");
+})
+
+</script>
+
+<style scoped>
+.title{
+  width: 800px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ src/store/all.ts
@@ -0,0 +1,35 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import hostname from "../env/hostname";
+import axios from "axios";
+import store from ".";
+import { useToast } from "primevue";
+
+interface AllItem{
+  title: string,
+  url: string
+}
+
+export default defineStore("all", ()=>{
+  const toast=useToast();
+  const list=ref<AllItem[]>([]);
+
+  const getList=async ()=>{
+    const {data: response}=await axios.get(`${hostname}/api/all/get`, {
+      headers: {
+        token: store().token,
+      }
+    })
+    if(response.ok){
+      const data=response.msg as AllItem[];
+      list.value=data;
+    }else{
+      toast.add({ severity: 'error', summary: '请求失败', detail: response.msg, life: 3000 });
+    }
+  }
+
+  return {
+    list,
+    getList,
+  }
+})
